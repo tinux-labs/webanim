d92524728ba580e9812f1fcce3e10236d9f3ff76
roblesdotdev <httpsnip@gmail.com>
2025-07-03 18:29:10 -0300

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
