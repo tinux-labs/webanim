40fa46680d6318267c212eeae9e6a01e90057149
roblesdotdev <httpsnip@gmail.com>
2025-07-06 23:00:22 -0300

    trailwing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://webanim.pages.dev",
+  trailingSlash: "always",
   server: {
     port: 3000,
   },
